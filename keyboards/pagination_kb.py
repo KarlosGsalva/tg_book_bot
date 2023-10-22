95be040d4b8f2666049ce6cd39f08bcf32731a06
Karlen M <karlenmusaelian@gmail.com>
2023-10-23 02:28:57 +0300

    fix pagination_kb, bookmarks_keyboard

--- keyboards/pagination_kb.py
+++ keyboards/pagination_kb.py
@@ -8,4 +8,6 @@ def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
     kb_builder = InlineKeyboardBuilder()
     kb_builder.row(*[InlineKeyboardButton(text=LEXICON[button] if button in LEXICON else button, callback_data=button)
                      for button in buttons])
+    # kb_builder.row(*[InlineKeyboardButton(text=LEXICON.get(button, button), callback_data=button)
+    #                  for button in buttons])
     return kb_builder.as_markup()
